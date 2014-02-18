6671bb29394b73e36672bef9321d979690846db9
Jraymond <Jraymond@student.dwc.edu>
2014-02-18 18:01:20 -0500

    Skeleton code for karel.cpp, update to input function
    filled out the code for karel .ccp
    updated the input function to better use karel data

--- Karel the robot/Karel the robot/Karel.cpp
+++ Karel the robot/Karel the robot/Karel.cpp
@@ -19,34 +19,63 @@ Karel::Karel()
 
 }
 
-Karel::Karel(int x, int y, int d, int b)
+Karel::Karel(int x, int y, int d, int b) // we can use this for unit tests, we will have to change it to read from a file for the final program
 {
+	std::cout<<"x ";
+	std::cin>>x;
+	std::cout<<"y= ";
+	std::cin>>y;
+	std::cout<<"d= ";
+    std::cin>>d;
+	std::cout<<"b= ";
+	std::cin>>b;
 
 }
 
-int Karel::karel_pos(int x, int y, int d)
+int Karel::karel_pos(int x, int y, int d) // same here as above
 {
+	std::cout<<"x ";
+	std::cin>>x;
+	std::cout<<"y ";
+	std::cin>>y;
+	std::cout<<"d ";
+	std::cin>>d;
 
+	return x, y, d;
 }
 
 int Karel::karel_beeper(int b)
 {
+	std::cout<<"b ";
+	std::cin>>b;
 
+	return b;
 }
 
 int Karel::get_xy(int x, int y)
 {
+	std::cout<<"x ";
+	std::cin>>x;
+	std::cout<<"y ";
+	std::cin>>y;
 
+	return x, y;
 }
 
 int Karel::get_d(int d)
 {
+	std::cout<<"d ";
+	std::cin>>d;
 
+	return d;
 }
 
 int Karel::get_b(int b)
 {
+	std::cout<<"b ";
+	std::cin>>b;
 
+	return b;
 }
 
 Karel::~Karel()
--- Karel the robot/Karel the robot/input function.cpp
+++ Karel the robot/Karel the robot/input function.cpp
@@ -4,12 +4,13 @@
 #include <iostream>
 #include <cstdlib>
 #include <string>
+#include "Karel.h"
 
 std::ifstream input_file;  // place holder names
 
 // no namespace for flexablity
 
-void get_input() // gets the input for the program
+int get_input() // gets the input for the program
 {
 	
 	char ans;
@@ -35,7 +36,24 @@ void get_input() // gets the input for the program
 		{
 		   if (i == 1)
 		   {
-			  //input karel data
+			   int temp[4];
+			  for (int i = 1; i<=4;i++)
+			  {
+				  
+				 input_file>>temp[i-1];
+
+
+			  }
+
+			  x = temp[0];
+			  y = temp[1];
+			  d = temp[2];
+			  b = temp[3];
+
+
+			   return x, y, d, b; 
+	// return here to stop the loop, when we call the function again it will start at 2 and return world data
+
 		   }
 		   else
 		   {
@@ -46,5 +64,5 @@ void get_input() // gets the input for the program
 	}
 
 	
-
+	return 0;
 }
